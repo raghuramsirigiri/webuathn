d3dc73ae59e0e1db01917950ae66f4d99a3b6057
raghuramsirigiri <raghuram9758@gmail.com>
2021-04-03 19:45:37 +0530

    initial commit

---
+++ static/js/webauthn.auth.js
@@ -0,0 +1,126 @@
+'use strict';
+/* Handle for register form submission */
+$('#register').submit(function(event) {
+    event.preventDefault();
+
+    let username = this.username.value;
+    let name     = this.name.value;
+
+    if(!username || !name) {
+        alert('Name or username is missing!')
+        return
+    }
+
+    getMakeCredentialsChallenge({username, name})
+        .then((response) => {
+            console.log("Server Response",response);
+            
+            let publicKey = preformatMakeCredReq(response);
+            // console.log("public key", publicKey);
+            
+            return navigator.credentials.create({ publicKey })
+        })
+        .then((response) => {
+            let makeCredResponse = publicKeyCredentialToJSON(response);
+            console.log("make cred response", makeCredResponse);
+            return sendWebAuthnResponse(makeCredResponse)
+        })
+        .then((response) => {
+            if(response.status === 'ok') {
+                loadMainContainer()   
+            } else {
+                alert(`Server responed with error. The message is: ${response.message}`);
+            }
+        })
+        .catch((error) => alert(error))
+    
+})
+
+        
+let getMakeCredentialsChallenge = (formBody) => {
+    return fetch('/webauthn/register', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formBody)
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        if(response.status !== 'ok')
+            throw new Error(`Server responed with error. The message is: ${response.message}`);
+
+        return response
+    })
+}
+
+
+let sendWebAuthnResponse = (body) => {
+    return fetch('/webauthn/response', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        if(response.status !== 'ok')
+            throw new Error(`Server responed with error. The message is: ${response.message}`);
+
+        return response
+    })
+}
+
+let getGetAssertionChallenge = (formBody) => {
+    return fetch('/webauthn/login', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formBody)
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        if(response.status !== 'ok')
+            throw new Error(`Server responed with error. The message is: ${response.message}`);
+
+        return response
+    })
+}
+
+/* Handle for login form submission */
+$('#login').submit(function(event) {
+    event.preventDefault();
+
+    let username = this.username.value;
+
+    if(!username) {
+        alert('Username is missing!')
+        return
+    }
+
+    getGetAssertionChallenge({username})
+        .then((response) => {
+            let publicKey = preformatGetAssertReq(response);
+            return navigator.credentials.get({ publicKey })
+        })
+        .then((response) => {
+            let getAssertionResponse = publicKeyCredentialToJSON(response);
+            console.log(getAssertionResponse);
+            
+            return sendWebAuthnResponse(getAssertionResponse)
+        })
+        .then((response) => {
+            if(response.status === 'ok') {
+                loadMainContainer()   
+            } else {
+                alert(`Server responed with error. The message is: ${response.message}`);
+            }
+        })
+        .catch((error) => alert(error))
+})
+
